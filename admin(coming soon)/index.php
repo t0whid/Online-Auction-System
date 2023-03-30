a373654420671222dedf436629c6e611ee1fd653
Nyakiamo Gregory <gregorynyakiamo7@gmail.com>
2023-03-30 23:06:22 +0300

    admin landing page

--- admin(coming soon)/index.php
+++ admin(coming soon)/index.php
@@ -1,1 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Online Auction System</title>
+	<link rel="stylesheet" type="text/css" href="css/homestyle.css">
 
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+	<div class="header">
+
+		<div class="container">
+			
+				<div class = "navbar">
+
+				<div class= "logo1">
+					<img src="item/newauction.png" width = "125px">
+					
+				</div>
+
+				<nav>
+					
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="login.php">Admin</a></li>
+					</ul>
+				</nav>
+			</div>
+			<div class = "row">
+				<div class = "col-2">
+					<h1>AuctionPro <br>Online Auction System</h1>
+					<p>Auctions Are Fun, Get Ready, Set, Run.</p>
+
+						
+
+				</div>
+				<div class="col-2">
+					<img src="item/newhome.png">
+				</div>
+
+			</div>
+		</div>	
+
+	</div>
+
+<!----featured categories---->
+
+
+</body>
+</html>
